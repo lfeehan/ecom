e969233e5dcd09c188eec9b70678474e7e2147d6
Peter Armstrong <peteretep@gmail.com>
2012-02-29 23:05:36 +0000

    php edited

--- index.php
+++ index.php
@@ -4,35 +4,26 @@
  <head> 
  <title>Kitesurfing Shop</title>
  <link href="ecom.css" rel="stylesheet" type="text/css" />
-
- 
- 
- </head>
- 
- <body>
- 
- <div id="container">
- 	<div id="header"><?php Echo "Header image here "."<BR>" ;?>
  	<?
 
     $user_name = "root";
     $password = "p";
     $database = "ecom";
-    $server = "127.0.0.1";
+    $server = "127.0.0.1"; 
 
 $db_handle = mysql_connect($server, $user_name, $password);
-$db_found = mysql_select_db($database, $db_handle);
-
-if ($db_found) {
-
+$db_found = mysql_select_db($database, $db_handle); 
 $SQL = "SELECT * FROM PRODUCTS";
 $result = mysql_query($SQL);
+ if ($db_found) {
 
-while ($db_field = mysql_fetch_assoc($result)) {
-print $db_field['PROD_ID'] . "<BR>";
-print $db_field['PROD_NAME'] . "<BR>";
-print $db_field['PROD_PRICE'] . "<BR>";
-print $db_field['PROD_QUANTITY'] . "<BR>";
+
+
+while ($db_field = mysql_fetch_assoc($result)) { 
+$prod_id= $db_field['PROD_ID'];
+$prod_name= $db_field['PROD_NAME'];
+$prod_price= $db_field['PROD_PRICE'];
+$prod_quantity= $db_field['PROD_QUANTITY'];
 }
 
 mysql_close($db_handle);
@@ -43,12 +34,21 @@ print "Database NOT Found ";
 mysql_close($db_handle);
 }
 
-?> </div>
+?>
+ 
+ 
+ </head>
+ 
+ <body>
+ 
+ <div id="container">
+ 	<div id="header"><?php Echo "Header image here "."<BR>" ;?>
+ </div>
  	<div id="breadcrumb" >
  	<? echo "breadcrumb" ?></div>
  	
  		<div id="fullpackage">
- 		<? echo "full package image link" ?></div>
+ 		<? echo "full package image link.<BR>"?> <? echo $prod_name; ?></div>
  		<div id="newproducts"><? echo "new products go here"?></div>
  		<div id="kites"><? echo "kites go here " ?></div>
  		<div id="boards"><?echo "boards go here" ?> </div>
@@ -58,4 +58,5 @@ mysql_close($db_handle);
 
 </div> <! container close div !>
 </body>
+<? mysql_close($db_handle); ?>
 </html>
