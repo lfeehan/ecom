52f1e82c76e3f9d9091dee9fa755a064d9f6bb7d
Peter Armstrong <peteretep@gmail.com>
2012-03-01 19:52:13 +0000

    oops added products.sql

--- PRODUCTS.sql
+++ PRODUCTS.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 29, 2012 at 06:11 PM
+-- Generation Time: Mar 01, 2012 at 07:49 PM
 -- Server version: 5.5.20
 -- PHP Version: 5.3.10-1ubuntu1
 
@@ -31,9 +31,18 @@ CREATE TABLE IF NOT EXISTS `PRODUCTS` (
   `PROD_NAME` varchar(20) NOT NULL,
   `PROD_PRICE` int(6) NOT NULL,
   `PROD_QUANTITY` int(3) NOT NULL,
-  `PROD_DESC` varchar(900) NOT NULL,
+  `PROD_DESC` text NOT NULL,
+  `PROD_TYPE` varchar(10) NOT NULL,
   PRIMARY KEY (`PROD_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `PRODUCTS`
+--
+
+INSERT INTO `PRODUCTS` (`PROD_ID`, `PROD_NAME`, `PROD_PRICE`, `PROD_QUANTITY`, `PROD_DESC`, `PROD_TYPE`) VALUES
+(1, 'test kite', 1000, 3, 'great kite', 'kite'),
+(2, 'test board', 500, 2, 'great board', 'board');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
