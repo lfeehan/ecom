92de0923c1f725cb2a98e8766e925ad48dfbd68f
Niall Brennan <niall.brennan33@mail.dcu.ie>
2012-04-17 14:50:07 +0100

    extra footer details

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <link href="ecom.css" rel="stylesheet" type="text/css" />
 
   <div id="footer"> 
+	 <small style= "float:left; padding-left:15px;"> Any queries? Please Email us at info@pantherkitesurfing.com or call us at 016786959</small>
     <small style= "float:right; padding-right:15px;"> (C) 2012 Niall, Derek, Peter, Lenny</small><br>
   </div>
 </body>
