f1f9868b1081fed39b9b2ddf442be9aaafd154db
Peter Armstrong <peteretep@gmail.com>
2012-04-17 17:25:54 +0100

    sorry

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 	 <small style= "float:left; padding-left:15px;"> Any queries? Please Email us at info@pantherkitesurfing.com or call us at 016786959</small>
     <small style= "float:right; padding-right:15px;"> (C) 2012 Niall, Derek, Peter, Lenny</small><br>
   </div>
-</div>  
+ 
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -20,97 +20,97 @@
 <div id="main-content"> 
 <?php include 'breadcrumb.php';?>	
 
-   	<div id="categories">
-	    <div id="kites"> <a href="products.php?prod_type=Kites">
-	      <img src="images/categoryImages/kitesurf_kites_button.png" style="border-style: none"/></a>
-	    </div>
-      <div id="boards"> <a href="products.php?prod_type=Boards">
-        <img src="images/categoryImages/kiteboards_button.png" style="border-style: none"/></a>
-      </div>
-      <div id="boards"> <a href="products.php?prod_type=Surfboards">
-        <img src="images/categoryImages/kite_surfboards_button.png" style="border-style: none"/></a>
-      </div>
-	    <div id="boards"> <a href="products.php?prod_type=Harnesses">
-     		<img src="images/categoryImages/harnesses_button.png" style="border-style: none"/></a>
-	    </div>
-	    <div id="accessories"><a href="products.php?prod_type=Accessories">
-	      <img src="images/categoryImages/accessories_button.png" style="border-style: none"/></a>
-	    </div>
+ 	<div id="categories">
+	  <div id="kites"> <a href="products.php?prod_type=Kites">
+	    <img src="images/categoryImages/kitesurf_kites_button.png" style="border-style: none"/></a>
+	  </div>
+    <div id="boards"> <a href="products.php?prod_type=Boards">
+      <img src="images/categoryImages/kiteboards_button.png" style="border-style: none"/></a>
     </div>
-
-  <div id="fullpackage">
-   	<a href="products.php?beginner=1"><img src="images/pantherpackage.jpg"></a>
+    <div id="boards"> <a href="products.php?prod_type=Surfboards">
+      <img src="images/categoryImages/kite_surfboards_button.png" style="border-style: none"/></a>
+    </div>
+	  <div id="boards"> <a href="products.php?prod_type=Harnesses">
+   		<img src="images/categoryImages/harnesses_button.png" style="border-style: none"/></a>
+	  </div>
+	  <div id="accessories"><a href="products.php?prod_type=Accessories">
+	    <img src="images/categoryImages/accessories_button.png" style="border-style: none"/></a>
+	  </div>
   </div>
-   	
-   		
-   		
-  <div id="newproducts" style="text-shadow: 5px 5px 20px #000; color: #fff; font-size: 16px;">
-    <?php 
-		  //new arrivals vs recently viewed
-			  $statement = "Recently Viewed";
+
+<div id="fullpackage">
+ 	<a href="products.php?beginner=1"><img src="images/pantherpackage.jpg"></a>
+</div>
+ 	
+ 		
+ 		
+<div id="newproducts" style="text-shadow: 5px 5px 20px #000; color: #fff; font-size: 16px;">
+  <?php 
+		//new arrivals vs recently viewed
+			$statement = "Recently Viewed";
 			
-			  if (!isset($_COOKIE['view']))
-			  {
-				  $statement = "New Products!";
-			  }
-			  echo "<center>";
-			  echo $statement;	
-			  //displaying newprods or recently viewed based on finding of cookie
+			if (!isset($_COOKIE['view']))
+			{
+				$statement = "New Products!";
+			}
+			echo "<center>";
+			echo $statement;	
+			//displaying newprods or recently viewed based on finding of cookie
 						
-			  if (isset($_COOKIE['view']))
-			  {
-				  $var1 = $_COOKIE['view'];	
-				  $newProds = queryDB("SELECT * FROM products WHERE prod_id='$var1' ORDER BY date_added DESC LIMIT 1");
-				  $newProds2 = queryDB("SELECT * FROM products ORDER BY date_added DESC LIMIT 1");
-			  }
+			if (isset($_COOKIE['view']))
+			{
+				$var1 = $_COOKIE['view'];	
+				$newProds = queryDB("SELECT * FROM products WHERE prod_id='$var1' ORDER BY date_added DESC LIMIT 1");
+				$newProds2 = queryDB("SELECT * FROM products ORDER BY date_added DESC LIMIT 1");
+			}
 
-			  if (!isset($_COOKIE['view']))
-			  {
-				  $newProds = queryDB("SELECT * FROM products ORDER BY date_added DESC LIMIT 2");
-			  }				
+			if (!isset($_COOKIE['view']))
+			{
+				$newProds = queryDB("SELECT * FROM products ORDER BY date_added DESC LIMIT 2");
+			}				
 					
-   			while ($one_row = mysql_fetch_assoc($newProds)) 
-			  { 
-			  $id= $one_row['prod_id'];
-			  $name= $one_row['name'];
+ 			while ($one_row = mysql_fetch_assoc($newProds)) 
+			{ 
+			$id= $one_row['prod_id'];
+			$name= $one_row['name'];
 			
-			  echo "<div style=\"float: top; padding: 5px; width:210px;\">";
+			echo "<div style=\"float: top; padding: 5px; width:210px;\">";
 			
-			  #this line generates a dynamic link based on product id, only one page "dynamic_product.php" handles all products
-			  echo "<a href = \"dynamic_product.php?product=" . $id . "\">" .  $name . "</a>";
+			#this line generates a dynamic link based on product id, only one page "dynamic_product.php" handles all products
+			echo "<a href = \"dynamic_product.php?product=" . $id . "\">" .  $name . "</a>";
 			
-			  $image_big = getImage($id);
-			  $image_resized = resizeImage($image_big, 250,250);
-			  echo "<img src =\"" . $image_resized . "\" height=75% width=75% style='border: 1px solid #000; -moz-border-radius: 5px; -webkit-border-radius: 5px; -moz-border-radius: 5px; -webkit-border-radius: 5px; border-radius: 5px;'>"; 
-			  echo "</div>";
-			  }
+			$image_big = getImage($id);
+			$image_resized = resizeImage($image_big, 250,250);
+			echo "<img src =\"" . $image_resized . "\" height=75% width=75% style='border: 1px solid #000; -moz-border-radius: 5px; -webkit-border-radius: 5px; -moz-border-radius: 5px; -webkit-border-radius: 5px; border-radius: 5px;'>"; 
+			echo "</div>";
+			}
 			
-			  if (isset($newProds2))
-			  {
-			    echo "<br/><br/>New Products!";
-			    while ($one_row = mysql_fetch_assoc($newProds2))
-				  { 
-     				$id= $one_row['prod_id'];
-     				$name= $one_row['name'];
-     				
-     				echo "<div style=\"float: top; padding: 5px; width:210px;\">";
-     				
-     				#this line generates a dynamic link based on product id, only one page "dynamic_product.php" handles all products
-     				echo "<a href = \"dynamic_product.php?product=" . $id . "\">" .  $name . "</a>";
-     				
-     				$image_big = getImage($id);
-     				$image_resized = resizeImage($image_big, 250,250);
-				    echo "<img src =\"" . $image_resized . "\" height=75% width=75% style='border: 1px solid #000; -moz-border-radius: 5px; -webkit-border-radius: 5px; -moz-border-radius: 5px; -webkit-border-radius: 5px; border-radius: 5px;'>"; 
-				    echo "</div>";
-				  }
-   			}
-   		?>	
-  </div>
+			if (isset($newProds2))
+			{
+			  echo "<br/><br/>New Products!";
+			  while ($one_row = mysql_fetch_assoc($newProds2))
+				{ 
+   				$id= $one_row['prod_id'];
+   				$name= $one_row['name'];
+   				
+   				echo "<div style=\"float: top; padding: 5px; width:210px;\">";
+   				
+   				#this line generates a dynamic link based on product id, only one page "dynamic_product.php" handles all products
+   				echo "<a href = \"dynamic_product.php?product=" . $id . "\">" .  $name . "</a>";
+   				
+   				$image_big = getImage($id);
+   				$image_resized = resizeImage($image_big, 250,250);
+				  echo "<img src =\"" . $image_resized . "\" height=75% width=75% style='border: 1px solid #000; -moz-border-radius: 5px; -webkit-border-radius: 5px; -moz-border-radius: 5px; -webkit-border-radius: 5px; border-radius: 5px;'>"; 
+				  echo "</div>";
+				}
+ 			}
+ 		?>	
+</div>
 
  		<!--prod_type variables are being passed through to the prodcuts.php
  		 page here -->
 
-		
+ 		
 
 <?php include 'footer.php';?>
 <? mysql_close($db_handle); ?>
